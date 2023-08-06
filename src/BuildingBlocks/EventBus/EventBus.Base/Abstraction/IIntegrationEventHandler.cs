77d9a96b8d2e11689fd3611eb2c49d804248dfed
canozdemir <canozdemir92@gmail.com>
2023-08-06 13:47:36 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using EventBus.Base.Events;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IIntegrationEventHandler<TIntegrationEvent> : IngtegrationEventHandler where TIntegrationEvent: IntegrationEvent
+    {
+       Task Handle(TIntegrationEvent @event);
+    }
+
+
+    public interface IngtegrationEventHandler
+    {
+
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubscriptionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EventBus.Base
+{
+    public class SubscriptionInfo
+    {
+        public Type HandlerType { get; set; }
+
+        public SubscriptionInfo(Type handlerType)
+        {
+            HandlerType = handlerType ?? throw new ArgumentNullException(nameof(handlerType));
+        }
+    }
+}
